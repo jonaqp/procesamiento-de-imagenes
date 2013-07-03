ba7e114f7d95da3df27601fe347db729f77b6e9a
Osvaldo Hinojosa <osvaldo.javier14@gmail.com>
2013-07-02 22:45:14 -0500

    Update mascaras.py
    Se agrego comentario por un error en git

--- veranos/tarea2/mascaras.py
+++ veranos/tarea2/mascaras.py
@@ -1,3 +1,4 @@
+''' Este archivo se debe llamar Mascaras.py, por x razon git lo cambia a minusulas. '''
 class Mascaras():
 	def __init__(self):
 		self.arr_sobelX = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
